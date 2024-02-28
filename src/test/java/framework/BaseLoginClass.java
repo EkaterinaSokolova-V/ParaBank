14c5296a03e31b676482dade67b611ce2944a6e0
EkaterinaSokolova-V <sokolovaekaterina.v@gmail.com>
2024-02-28 16:13:27 +0100

    BaseLoginClass added1

--- src/test/java/framework/BaseLoginClass.java
+++ src/test/java/framework/BaseLoginClass.java
@@ -1,18 +1,10 @@
 package framework;
-
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class BaseLoginClass extends BaseTest{
+public class BaseLoginClass extends BaseClass{
     @BeforeMethod
     public void beforeMethod() {
         initialization();
         login();
-    }
-
-    @AfterMethod
-    public void afterMethod() {
-        getDriver().close();
-        getDriver().quit();
     }
 }
